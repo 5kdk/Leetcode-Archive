64ef76b8d9e59357fa7b40c4a50877e264f887ff
Dongkyu Kim <86090355+5kdk@users.noreply.github.com>
2022-11-15 19:33:19 +0900

    Time: 85 ms (77.13%), Space: 42.1 MB (85.89%) - LeetHub

--- Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ Medium/0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -5,12 +5,11 @@
 var maxProfit = function (prices) {
   let profit = 0;
 
-  for (let i = 1; i < prices.length; i++) {
-    let prePrice = prices[i - 1];
-    let curPrice = prices[i];
-    if (prePrice < curPrice) {
-      profit += curPrice - prePrice;
+  for (let i = 0; i < prices.length - 1; i++) {
+    if (prices[i] < prices[i + 1]) {
+      profit += prices[i + 1] - prices[i];
     }
   }
+
   return profit;
 };
