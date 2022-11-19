6cffd10443a127d0b103dc134d997be2892519b2
Dongkyu Kim <86090355+5kdk@users.noreply.github.com>
2022-11-19 18:44:23 +0900

    Time: 170 ms (32.46%), Space: 46.8 MB (36.19%) - LeetHub

--- Easy/0283-move-zeroes/0283-move-zeroes.js
+++ Easy/0283-move-zeroes/0283-move-zeroes.js
@@ -7,7 +7,7 @@ var moveZeroes = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     if (nums[i] !== 0) {
       nums[idx] = nums[i];
-      nums[i] = idx === i ? nums[i] : 0;
+      if(idx !== i) nums[i] = 0;
       idx++;
     }
   }
