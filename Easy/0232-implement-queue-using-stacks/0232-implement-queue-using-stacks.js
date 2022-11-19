0bf91550ce5469139c1a4f59c36777f8150ae49c
Dongkyu Kim <86090355+5kdk@users.noreply.github.com>
2022-11-19 19:40:17 +0900

    Time: 98 ms (35.46%), Space: 42.1 MB (42.86%) - LeetHub

--- Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -1,39 +1,47 @@
-var MyQueue = function () {
-  this.stack1 = [];
-  this.stack2 = [];
+var MyQueue = function() {
+    this.stack1 = []
+    this.stack2 = []
+    this.front = null
 };
 
-/**
+/** 
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function (x) {
-  this.stack1.push(x);
+MyQueue.prototype.push = function(x) {
+    if(this.stack1.length == 0) {
+        this.front = x
+    }
+    this.stack1.push(x)
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.pop = function () {
-  return this.stack1.shift();
+MyQueue.prototype.pop = function() {
+    if(this.stack2.length == 0) {
+        while(this.stack1.length !=0) {
+            this.stack2.push(this.stack1.pop())
+        }
+    }
+    return this.stack2.pop()
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.peek = function () {
-  this.stack2[0] = this.stack1[0];
-  return this.stack2[0];
+MyQueue.prototype.peek = function() {
+    return this.stack2.length == 0 ? this.front : this.stack2[this.stack2.length-1]
 };
 
 /**
  * @return {boolean}
  */
-MyQueue.prototype.empty = function () {
-  return this.stack1.length === 0 ? true : false;
+MyQueue.prototype.empty = function() {
+    return this.stack1.length == 0 && this.stack2.length == 0
 };
 
-/**
+/** 
  * Your MyQueue object will be instantiated and called as such:
  * var obj = new MyQueue()
  * obj.push(x)
