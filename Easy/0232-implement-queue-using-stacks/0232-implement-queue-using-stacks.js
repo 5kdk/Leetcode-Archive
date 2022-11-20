4baf02bf682d9eb59ca9c1cd2f48d87a0b4092f4
Dongkyu Kim <86090355+5kdk@users.noreply.github.com>
2022-11-20 21:46:43 +0900

    Time: 61 ms (94.24%), Space: 41.9 MB (67.02%) - LeetHub

--- Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
+++ Easy/0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.js
@@ -1,47 +1,58 @@
-var MyQueue = function() {
-    this.stack1 = []
-    this.stack2 = []
-    this.front = null
+var MyQueue = function () {
+  this.stack1 = [];
+  this.stack2 = [];
 };
 
-/** 
+/**
  * @param {number} x
  * @return {void}
  */
-MyQueue.prototype.push = function(x) {
-    if(this.stack1.length == 0) {
-        this.front = x
-    }
-    this.stack1.push(x)
+MyQueue.prototype.push = function (x) {
+  this.stack1.push(x);
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.pop = function() {
-    if(this.stack2.length == 0) {
-        while(this.stack1.length !=0) {
-            this.stack2.push(this.stack1.pop())
-        }
-    }
-    return this.stack2.pop()
+MyQueue.prototype.pop = function () {
+  while (this.stack1.length !== 0) {
+    this.stack2.push(this.stack1.pop());
+  }
+
+  const pop = this.stack2.pop();
+
+  while (this.stack2.length !== 0) {
+    this.stack1.push(this.stack2.pop());
+  }
+
+  return pop;
 };
 
 /**
  * @return {number}
  */
-MyQueue.prototype.peek = function() {
-    return this.stack2.length == 0 ? this.front : this.stack2[this.stack2.length-1]
+MyQueue.prototype.peek = function () {
+  while (this.stack1.length !== 0) {
+    this.stack2.push(this.stack1.pop());
+  }
+
+  const pop = this.stack2.pop();
+  this.stack2.push(pop);
+  while (this.stack2.length !== 0) {
+    this.stack1.push(this.stack2.pop());
+  }
+
+  return pop;
 };
 
 /**
  * @return {boolean}
  */
-MyQueue.prototype.empty = function() {
-    return this.stack1.length == 0 && this.stack2.length == 0
+MyQueue.prototype.empty = function () {
+  return this.stack1.length === 0 ? true : false;
 };
 
-/** 
+/**
  * Your MyQueue object will be instantiated and called as such:
  * var obj = new MyQueue()
  * obj.push(x)
